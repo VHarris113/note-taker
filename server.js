4d5e0843c6edd79096754a26b81c4eaa7a5391c1
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-15 10:40:18 -0400

    trying to fix style sheets for note.html

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const PORT = process.env.PORT || 9000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('./public/index', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
-app.get('./public/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
 
 
